90dc472156b8e8a7307d99674928cb4bc22bc43c
Ramakrishna <lrk.coding@gmail.com>
2023-07-03 14:14:45 -0500

    useEffect,useTextEditingController,useState

--- lib/main1_useStream.dart
+++ lib/main1_useStream.dart
@@ -14,30 +14,21 @@ void main() {
   );
 }
 
+Stream<String> getTime() => Stream.periodic(
+      const Duration(seconds: 1),
+      (_) => DateTime.now().toIso8601String(),
+    );
+
 class HomePage extends HookWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final controller = useTextEditingController();
-    final text = useState('');
-    useEffect(
-      () {
-        controller.addListener(() {
-          text.value = controller.text;
-        });
-        return null;
-      },
-      [controller],
-    );
+    final dateTime = useStream(getTime());
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: Text(dateTime.data ?? 'Home Page'),
       ),
-      body: Column(children: [
-        TextField(controller: controller),
-        Text('You typed ${text.value}'),
-      ]),
     );
   }
 }
